d123f8386385d466fe1cca0c9377e4b2db6ce8b1
chavand.aurelien <chavand.aurelien@gmail.com>
2024-12-03 08:51:58 +0100

    Commit #2

---
+++ mcarre.cpp
@@ -0,0 +1,37 @@
+#include "carre.h"
+
+
+
+class CCarre
+{
+
+private :
+
+	int sx, sy, cote;
+
+};
+
+
+
+
+
+
+
+
+
+/*
+class Temps
+{
+public :
+Temps();
+~Temps();
+void ajusterTemps( int, int, int);
+void afficherMilitaire();
+void afficherStandard ();
+private :
+int heure;
+int minute;
+int seconde;
+};
+*/
+ 

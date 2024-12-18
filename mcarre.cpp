3042fe4867d2bcb2eadd0fb91c321b64a121c223
chavand.aurelien <chavand.aurelien@gmail.com>
2024-12-18 17:23:55 +0100

    Commit #6

--- mcarre.cpp
+++ mcarre.cpp
@@ -19,7 +19,25 @@ int main()
 	c1.Afficher();
 	c1.Deplacer(5, -3);
 	c1.Afficher();
-	CCarre TabCarre[3]; 
+	CCarre TabCarre[4]; 
+
+	TabCarre[1].Setsx(4);
+	TabCarre[1].Setsy(17);
+	TabCarre[1].Setcote(95);
+	TabCarre[1].Afficher();
+	TabCarre[2].Setsx(7);
+	TabCarre[2].Setsy(19);
+	TabCarre[2].Setcote(93);
+	TabCarre[2].Afficher();
+	TabCarre[3].Setsx(2);
+	TabCarre[3].Setsy(23);
+	TabCarre[3].Setcote(105);
+	TabCarre[3].Afficher();
+	TabCarre[4].Setsx(5);
+	TabCarre[4].Setsy(12);
+	TabCarre[4].Setcote(115);
+	TabCarre[4].Afficher();
+
 	/*
 	
 	TabCarre[].Setsx(3);
@@ -42,8 +60,6 @@ int main()
 	
 	
 
-
-
-	
 }
+
 
